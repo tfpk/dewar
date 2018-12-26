ff723e3727984c2e559da5ccd25449d3792f0957
Tom Kunc <tomkunc0@gmail.com>
2018-12-26 13:44:12 +1100

    feat(setup.py): version bump

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='dewar',
-    version='0.1',
+    version='0.1.1',
     description='Static Site Generator, like flask',
     url='http://github.com/tfpk/dewar',
-    download_url='http://github.com/tfpk/dewar/archive/v0.1.0.tar.gz',
+    download_url='http://github.com/tfpk/dewar/archive/v0.1.1.tar.gz',
     author='tfpk',
     author_email='tomkunc0@gmail.org',
     license='All Rights Reserved',
