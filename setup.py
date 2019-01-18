09efc448d7bbca66e1409017d473a5b45fd61dd1
Tom Kunc <tomkunc0@gmail.com>
2019-01-18 18:18:40 +1100

    fix(setup): change version.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/tfpk/dewar'
 EMAIL = 'tomkunc0@gmail.com'
 AUTHOR = 'tfpk'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.1.4'
+VERSION = '1.1.5'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
