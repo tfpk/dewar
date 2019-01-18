12be780700b21350c8eef43319ab9a70486185b6
Tom Kunc <tomkunc0@gmail.com>
2019-01-18 18:03:45 +1100

    fix(static_url): change warning syntax.

--- dewar/helpers.py
+++ dewar/helpers.py
@@ -226,7 +226,7 @@ def static_url(path, start=None):
     :rtype: str
     """
     if not (site.static_path / Path(path)).is_file():
-        raise warnings.warn('Could not find the path given.')
+        raise warnings.warn('Could not find the path given.', Warning)
     return rel_url_to(site.static_path, start=start)
 
 
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/tfpk/dewar'
 EMAIL = 'tomkunc0@gmail.com'
 AUTHOR = 'tfpk'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.1.2'
+VERSION = '1.1.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
